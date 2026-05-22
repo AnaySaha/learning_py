137d9af960aa5af38c8dccb5e280617e36ecee16
Anay Saha <anaysahapaban@gmail.com>
2026-05-22 21:00:53 +0600

    add

---
+++ chapter1/problem1.py
@@ -0,0 +1,27 @@
+print('''
+Let me introduce you to my party people in the club, huh
+I'm loose
+And everybody knows I get off the chain
+Baby it's the truth, it's the truth
+I'm like Inception
+I play with your brain
+So don't sleep or snooze
+I don't play no games so don't, don't, don't
+Don't get it confused no
+'Cause you will lose yeah
+Now, no pu-pu-pu-pu-pump it up
+And back it up like a Tonka truck
+Dale
+If you go hard you gotta get on the floor
+If you're a party freak then step on the floor
+If you're an animal then tear up the floor
+Break a sweat on the floor
+Yeah we work on the floor
+Don't stop, keep it movin'
+Put your drinks up
+Pick your body up and drop it on the floor
+Let the rhythm change your world on the floor
+You know we're running shit tonight on the floor
+Brazil, Morocco, London to Ibiza
+Straight to LA, New York, Vegas to Africa
+''')
---
+++ chapter2/rules_variables.py
@@ -0,0 +1,13 @@
+aaa = 435
+
+harry = 34
+
+sameer = 45
+
+_samerr = 34
+
+
+# @sameer = 56 # Invalid due to @ symbol
+
+# S@meer # Invalid due to @ symbol
+
