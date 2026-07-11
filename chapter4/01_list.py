b47844e2693b3b62631ad8a30ddff49211986b41
Anay Saha <anaysahapaban@gmail.com>
2026-07-11 22:58:43 +0600

    2nd

--- chapter4/01_list.py
+++ chapter4/01_list.py
@@ -1,3 +1,8 @@
 friends = ["Apple", "Orange", 5, 345.06, False, "Aakash", "Rohan"]
 
 print(friends[0])
+
+friends[0] = "Grapes" # Unlike strings list
+
+print(friends[0])
+print(friends[1:4])
---
+++ chapter4/02_list_methods.py
@@ -0,0 +1,12 @@
+friends = ["Apple", "Orange", 5, 345.06, False, "Aakash", "Rohan"]
+print(friends)
+friends.append("Anay")
+print(friends)
+
+
+l1 = [1,2,34,56,6,11]
+l1.sort() #l1.reverse()
+l1.insert(3,33333) #Insert
+vaule = l1.pop(3)
+print(vaule)
+print(l1)
